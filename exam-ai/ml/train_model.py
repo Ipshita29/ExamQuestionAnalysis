0d266b188bc80862066ff8d26a23d47ea4dbb0ad
[Harshit Gupta] <hrstgpt87@gmail.com>
2026-02-22 22:21:56 +0530

    PKL

--- exam-ai/ml/train_model.py
+++ exam-ai/ml/train_model.py
@@ -18,7 +18,7 @@ X_vec = vectorizer.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X_vec, y, test_size=0.2, random_state=42)
 
-model = LogisticRegression(max_iter=200)
+model = LogisticRegression(class_weight="balanced",max_iter=200)
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
